8c2d8447eeb64cac17d6018d093f2a8726246f34
dx0naclyoo <konyzhenkovdanil@gmail.com>
2024-03-07 08:40:20 +0300

    Add more endpoint's

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI()  # docs_url=None, redoc_url=None | Settings for deployment on prod
 
 
 @app.get("/")
 async def mainpage():
     return {"message": "successful connect"}
+
+
+@app.get("/item/{itemid}")
+async def get_item(itemid: int):
+    return {"message": f"{itemid}"}
--- src/backend/settings.py
+++ src/backend/settings.py
@@ -1,4 +1,12 @@
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class DatabaseSettings(BaseSettings):
+    pass
 
 
 class AppSettings(BaseSettings):
